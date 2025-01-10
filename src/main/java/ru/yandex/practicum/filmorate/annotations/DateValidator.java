9c2d0c833a0fff2c29f91a8fda8820d567b0e434
Nikita Zubov <n.zubov@lipt-soft.ru>
2025-01-10 19:00:07 +0300

    sprint-11: Fix date validator

--- src/main/java/ru/yandex/practicum/filmorate/annotations/DateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotations/DateValidator.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum.filmorate.annotations;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-
 
 import java.time.LocalDate;
 
@@ -13,9 +11,6 @@ public class DateValidator implements ConstraintValidator<ReleaseDate, LocalDate
 
     @Override
     public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
-        if (value.isBefore(FIRST_RELEASE_DATE)) {
-            throw new ValidationException("The release date is no earlier than December 28, 1895");
-        }
-        return true;
+        return value == null || !value.isBefore(FIRST_RELEASE_DATE);
     }
 }
